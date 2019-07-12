be960424eabe4404da0c37602715ca37bc9a61b2
David Alfageme <alfalfageme@gmail.com>
2019-07-12 13:45:41 +0200

    Basic todo project con multiple lists ( like trello )

---
+++ 10todo/src/components/TaskList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="list">
+    <h1>{{list.title}}</h1>
+    <ul>
+      <li v-for="todo of list.todos" :key="todo.id">{{todo.name}}</li>
+    </ul>
+    <div>
+      <input v-model="text">
+      <button @click="add">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Esto es una lisat',
+      text: '',
+    };
+  },
+  methods: {
+    add() {
+      console.log(this.text);
+      this.list.todos.push({
+        name: this.text,
+        done: false,
+        id: this.list.todos.length,
+      });
+      this.text = '';
+    },
+  },
+  props: {
+    list: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style>
+  .list{
+    width: 200px;
+    text-align: left;
+  }
+</style>
---
+++ 10todo/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="lists">
+    <TaskList v-for="list of tasksLists" :key="list.id"  :list="list"/>
+    <div class="new">
+      <input v-model="listTitle"/>
+      <button @click="addList">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TaskList from '@/components/TaskList.vue';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      listTitle: '',
+      tasksLists: [
+        {
+          id: 0,
+          title: 'Fake',
+          todos: [
+            {
+              name: 'Este es mi primer todo',
+              done: false,
+              id: 0,
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    addList() {
+      this.tasksLists.push({
+        title: this.listTitle,
+        id: this.tasksLists.length,
+        todos: [],
+      });
+    },
+  },
+  components: {
+    TaskList,
+  },
+};
+</script>
+
+<style>
+  .lists{
+    display: flex;
+    flex-direction: row;
+  }
+</style>
